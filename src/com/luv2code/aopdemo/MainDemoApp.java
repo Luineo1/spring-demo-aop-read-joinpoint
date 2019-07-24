a2c3bf23f76a86606def300c2ef009d3e0a050e0
chjinlook <chjinlook@gmail.com>
2019-07-24 16:21:54 +0900

    When we are in an aspect (ie for logging) • How can we access method parameters?

--- src/com/luv2code/aopdemo/MainDemoApp.java
+++ src/com/luv2code/aopdemo/MainDemoApp.java
@@ -20,6 +20,9 @@ public class MainDemoApp {
 		
 		// call the business method
 		Account myAccount = new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
+		
 		theAccountDAO.addAccount(myAccount,true);
 		theAccountDAO.doWork();
 		
